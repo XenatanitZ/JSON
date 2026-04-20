caabccf1491850639960f308b86dd6413aca7304
ZhuchkaTriplesix <mrlololoshka94@gmail.com>
2026-04-20 22:07:50 +0300

    style: format + check ruff

--- JSON/custom_errors.py
+++ JSON/custom_errors.py
@@ -1,19 +1,20 @@
-from pydantic import BaseModel, ValidationError, field_validator
-
 class UserAlreadyExistsError(Exception):
     def __init__(self, username: str):
         self.username = username
         super().__init__(f"Пользователь {username} уже существует")
+
 
 class UserNotFoundError(Exception):
     def __init__(self, username: str):
         self.username = username
         super().__init__(f"Пользователь {username} не найден")
 
+
 class AddingUserError(Exception):
     def __init__(self, username: str):
         self.username = username
         super().__init__(f"Ошибка при добавлении пользователя {username}")
+
 
 class DeletingUserError(Exception):
     def __init__(self, username: str):
